b30dbd37ca3769eeed8707d358df0d12f0b0f5f9
Sankalp-Dawada <sankalpdawada@gmail.com>
2025-01-16 03:38:48 +0530

    Changes made on theme page and currency convertor while there still some major bugs need to be sort out by me asap logic building pending in most of the part pls don't judge my ui it's 3 38 feeling very dizzy

--- lib/pages/settingsPages/currencyconvertor.dart
+++ lib/pages/settingsPages/currencyconvertor.dart
@@ -2,18 +2,233 @@ import 'package:flutter/material.dart';
 class Currencyconvertor extends StatefulWidget {
   const Currencyconvertor({super.key});
 
+
+
   @override
   State<Currencyconvertor> createState() => _CurrencyconvertorState();
 }
 
 class _CurrencyconvertorState extends State<Currencyconvertor> {
+  
+  String initial_dropdownvalue1 = 'INR(₹)';
+  String initial_dropdownvalue2 = 'US Dollar(\$)';
+  int? result;
+
+final Map<String, Map<String, double>> _exchangeRates = { 
+    'INR(₹)': {
+      'US Dollar(\$)': 0.012, 
+      'Euro(€)': 0.011,
+      'Pounds(£)': 0.0098,
+      'Yen(¥)': 1.7,
+      'INR(₹)': 1.0, 
+    },
+    'US Dollar(\$)': {
+      'INR(₹)': 82.0,  
+      'Euro(€)': 0.91,
+      'Pounds(£)': 0.82,
+      'Yen(¥)': 140.0,
+      'US Dollar(\$)': 1.0,
+    },
+    'Euro(€)':{
+      'INR(₹)': 88.92, 
+      'Euro(€)': 1.0,
+      'Pounds(£)': 0.84,
+      'Yen(¥)': 161.08,
+      'US Dollar(\$)': 1.03,
+    },
+    'Pounds(£)':{
+      'INR(₹)': 105.69,  
+      'Euro(€)': 1.19,
+      'Pounds(£)': 1.0,
+      'Yen(¥)': 191.37,
+      'US Dollar(\$)': 1.22,
+    },
+    'Yen(¥)':{
+      'INR(₹)': 0.55,  
+      'Euro(€)': 0.0062,
+      'Pounds(£)': 0.0052,
+      'Yen(¥)': 1.0,
+      'US Dollar(\$)': 0.0064,
+    },
+  };
+
+  double _amount = 0.0; 
+  String _result = '0.0';
+
   @override
   Widget build(BuildContext context) {
+
+     Widget dropDownVariablefrom = DropdownButton<String>(
+      value: initial_dropdownvalue1,
+      onChanged: (String? newValue) {
+        setState(() {
+          initial_dropdownvalue1 = newValue!;
+        });
+      },
+      items: <String>[
+        'INR(₹)',
+        'Euro(€)',
+        'US Dollar(\$)',
+        'Pounds(£)',
+        'Yen(¥)',
+        ]
+  .map<DropdownMenuItem<String>>((String value){
+    return DropdownMenuItem<String>(
+      value: value,
+      child: Text(value),
+    );
+  }).toList(),
+);
+
+Widget dropDownVariableTo =DropdownButton<String>(
+      value: initial_dropdownvalue2,
+      onChanged: (String? newValue) {
+        setState(() {
+          initial_dropdownvalue2 = newValue!;
+        });
+      },
+      items: <String>[
+        'US Dollar(\$)',
+        'Euro(€)',
+        'INR(₹)',
+        'Pounds(£)',
+        'Yen(¥)',
+        ]
+  .map<DropdownMenuItem<String>>((String value){
+    return DropdownMenuItem<String>(
+      value: value,
+      child: Text(value),
+    );
+  }).toList(),
+);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Currency Converter"),
         backgroundColor: Colors.grey[300],
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Amount',
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 16,
+                            ),
+                          
+                          ),
+                        SizedBox(height: 8,),
+                        TextField(
+                           keyboardType: TextInputType.number, 
+                          onChanged: (value) {
+                            setState(() {
+                              _amount = double.tryParse(value) ?? 0.0; 
+                            });
+                          },
+                          decoration: InputDecoration(
+                            label: Text('Enter Amount'),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    Column(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'From',
+                              style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 16,
+                              ),),
+                            dropDownVariablefrom,
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        IconButton(
+                          onPressed: (){
+                            setState(
+                              () {
+                          String temp = initial_dropdownvalue1;
+                          initial_dropdownvalue1 = initial_dropdownvalue2;
+                          initial_dropdownvalue2 = temp;
+                          }
+                          );
+                          },
+                          icon: Icon(Icons.swap_vert),
+                          ),
+                        SizedBox(height: 20,),
+                        Column(
+                          children: [
+                            Text(
+                              'To',
+                            style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 16,
+                              ),),
+                            dropDownVariableTo,
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_exchangeRates.containsKey(initial_dropdownvalue1) &&
+                                _exchangeRates[initial_dropdownvalue1]!
+                                    .containsKey(initial_dropdownvalue2)) {
+                              final double rate =
+                                  _exchangeRates[initial_dropdownvalue1]![initial_dropdownvalue2]!;
+                              _result = (_amount * rate).toStringAsFixed(2);
+                            } else {
+                              _result = 'Rate not found'; 
+                              print(
+                                  'Error: Rate not found for $initial_dropdownvalue1 to $initial_dropdownvalue2'); 
+                            }
+                          });
+                        },
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                          ),
+                          child: Text('Convert'),
+                        ),
+        
+                        SizedBox(height: 20),
+                        Container(
+                          padding: const EdgeInsets.all(15), 
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Text(_result),
+                           
+                        ),
+                      ],
+        
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          ),
+      )
     );
   }
 }
--- lib/pages/settingsPages/theme.dart
+++ lib/pages/settingsPages/theme.dart
@@ -7,12 +7,451 @@ class Themes extends StatefulWidget {
 }
 
 class _ThemesState extends State<Themes> {
+
+  bool isDarkModeEnabled = false;
+  double fontSize = 16;
+  bool showDecimals = true;
+  String dropdownValue = 'Line';
+  bool showGridLines = true;
+  Color primaryColor = Colors.blue;
+
+  void _applyChanges() {
+    // TODO: logic to apply the selected theme settings.
+
+    print('Dark Mode: $isDarkModeEnabled');
+    print('Font Size: $fontSize');
+    print('Show Decimals: $showDecimals');
+    print('Chart Type: $dropdownValue');
+    print('Show Grid Lines: $showGridLines');
+    print('Primary Color: $primaryColor');
+
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Changes saved!')),
+    );
+  }
+
+
+  void _resetToDefaults() {
+    setState(() {
+      isDarkModeEnabled = false;
+      fontSize = 16;
+      showDecimals = true;
+      dropdownValue = 'Line';
+      showGridLines = true;
+      primaryColor = Colors.blue;
+    });
+
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Settings reset to defaults!')),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Themes"),
         backgroundColor: Colors.grey[300],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(100, 26, 35, 126)
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Customize Theme',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Make the app truly yours',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontStyle: FontStyle.italic,
+                    ),
+                    ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 153, 176, 187),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Color Scheme',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Primary Color',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  'Used for main elements',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                  ),
+                                  ),
+                              ],
+                            ),
+                            SizedBox(width: 10,),
+                            Container( // Primary color picker
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              color: primaryColor,
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Select Primary Color'),
+                                    content: SingleChildScrollView( // Use SingleChildScrollView for potentially long list
+                                      child: Column(
+                                        children: [
+                                          for (final color in [Colors.red, Colors.green, Colors.blue, Colors.orange, Colors.purple, Colors.teal /* ...add more as needed */])
+                                            GestureDetector(
+                                              onTap: () {
+                                                setState(() {
+                                                  primaryColor = color;
+                                                  Navigator.pop(context); // Close the dialog
+                                                });
+                                              },
+                                              child: Container(
+                                                margin: const EdgeInsets.symmetric(vertical: 4),
+                                                width: 50,
+                                                height: 50,
+                                                decoration: BoxDecoration(
+                                                  color: color,
+                                                  shape: BoxShape.circle,
+                                                ),
+                                              ),
+                                            ),
+
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                              color: Colors.blue,
+                                borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                        ),
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 153, 176, 187),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Dark Mode',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Enable Dark Mode',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  'Easier on eyes at night',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                  ),
+                                  ),
+                              ],
+                            ),
+                            SizedBox(width: 10,),
+                            Switch(
+                              value: isDarkModeEnabled, // Replace with your actual variable
+                              onChanged: (value) {
+                                setState(() {
+                                  isDarkModeEnabled = value;
+                                  // TODO: Implement logic to change app theme
+                                });
+                              },
+                            ),
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 153, 176, 187),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Font Size',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                                Text(
+                                  'Font Size',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  'x10',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                  ),
+                                  ),
+                            SizedBox(height: 10,),
+                            
+                            Slider(
+                            value: fontSize, // Your font size variable
+                            min: 10, // Minimum font size
+                            max: 30, // Maximum font size
+                            onChanged: (newValue) {
+                              setState(() {
+                                fontSize = newValue;
+                                // TODO: Apply the new font size
+                                });
+                              },
+                            ),
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 153, 176, 187),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Currency Display',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                                Text(
+                                  'Show Decimals',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                            Switch(
+                            value: showDecimals, // Your boolean variable
+                            onChanged: (value) {
+                              setState(() {
+                                showDecimals = value;
+                                // TODO: Implement logic to handle decimal display
+                              });
+                            },
+                          ),
+
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Currency Symbol'),
+                            Text('₹'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 153, 176, 187),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Chart Preferences',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Text(
+                                          'Default Chart Type',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                          ),
+                                        ),
+
+
+                                        DropdownButton<String>(
+                                          value: dropdownValue,
+                                          onChanged: (String? newValue) {
+                                            setState(() {
+                                              dropdownValue = newValue!;
+                                              // TODO: Apply the selected chart type
+                                            });
+                                          },
+                                          items: <String>['Line', 'Bar', 'Pie'] // Example chart types
+                                              .map<DropdownMenuItem<String>>((String value) {
+                                            return DropdownMenuItem<String>(
+                                              value: value,
+                                              child: Text(value),
+                                            );
+                                          }).toList(),
+                                        ),
+
+                                      ],
+                                    ),
+                                    
+                                    Row(
+                                      children: [
+                                        Text(
+                                          'Show Grid Lines',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                        
+                                        Switch(
+                                          value: showGridLines, // Your boolean variable
+                                          onChanged: (value) {
+                                            setState(() {
+                                              showGridLines = value;
+                                              // TODO: Implement logic to toggle grid lines on the chart
+                                            });
+                                          },
+                                        ),
+
+
+                                      ],
+                                    ),
+                                     SizedBox(width: 10,),
+                                  ],
+                                ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  ElevatedButton(
+                    onPressed: _applyChanges, 
+                    style: ButtonStyle(
+                      foregroundColor: WidgetStatePropertyAll(Colors.white),
+                      backgroundColor: WidgetStatePropertyAll(Colors.grey),
+                    ),
+                    child: Text('Save Changes'),
+                    ),
+                    SizedBox(height: 5,),
+                  ElevatedButton(
+                    onPressed: _resetToDefaults,
+                    style: ButtonStyle(
+                      // backgroundBuilder: WidgetStatePropertyAll(Colors.grey),
+                      backgroundColor: WidgetStatePropertyAll(Colors.white),
+                    ), 
+                    child: Text('Reset to Defaults'),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
