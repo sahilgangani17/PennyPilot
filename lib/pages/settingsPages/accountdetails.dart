7e742264d3c3bf244c39edd4e9b094b1001e5c66
Sankalp-Dawada <sankalpdawada@gmail.com>
2025-01-16 20:32:33 +0530

    UI of Account detail changed

--- lib/pages/settingsPages/accountdetails.dart
+++ lib/pages/settingsPages/accountdetails.dart
@@ -8,6 +8,7 @@ class Accountdetails extends StatefulWidget {
 }
 
 class _AccountdetailsState extends State<Accountdetails> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,151 +16,224 @@ class _AccountdetailsState extends State<Accountdetails> {
         title: const Text("Account Details"),
         backgroundColor: Colors.grey[300],
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20)
-            ),
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      decoration:BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Profile Pic , Name and Email
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20)
+              ),
+              child: Column(
+                children: [
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration:BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                        ),
+                        //TODO: Image profile upload from local device
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
+                              child: Image.network(
+                                'https://wallpapers.com/images/hd/white-duck-face-close-up-ybtrbmo0scwowgn1.jpg',
+                                fit: BoxFit.cover,
+                                width: 100,
+                                height: 100,
+                              ),
+                            ),
+                          ],
+                        ),
+        
                       ),
-                      // child: Text('Hello'),
-                      child: ClipOval(
-                        child: Image.network(
-                          'https://wallpapers.com/images/hd/white-duck-face-close-up-ybtrbmo0scwowgn1.jpg',
-                          fit: BoxFit.cover,
-                          width: 100,
-                          height: 50,
+                      SizedBox(height: 20,),
+                      Text(
+                        '(UserName)',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-
-                    )
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Name'),
-                    Text('emailf'),
-                  ],
-                )
-              ],
+                      Text(
+                        '(Email)',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.grey,
+                          ),
+                        ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 20,),
-          Container(
-            decoration:BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.grey,
-            ),
-            child: Column(
-              children: [
-                Text('Login Details'),
-                Column(
-                  children: [
-                    Column(
-                      children: [
-                        Text('Email:'),
-                        Text('abc@gmail.com')
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text('Password'),
-                        Row(
-                          children: [
-                            Text('abc@1234'),
-                            ElevatedButton(onPressed: (){}, child:Text('Change'))
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ) ,
-          ),
-          SizedBox(height: 20,),
-          Container(
-            decoration:BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.grey,
-            ),
-            child:Column(
-              children: [
-                Text('Personal Details'),
-                Column(
-                  children: [
-                    Column(
-                      children: [
-                        Text('Phone Number'),
-                        Text('1234567890')
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text('DOB'),
-                        Text('13/8/1997')
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text('Address'),
-                        Text('abcdefghijklmnopqrstuvwxyz')
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ) ,
-          ),
-          SizedBox(height: 20,),
-          Container(
+            SizedBox(height: 20,),
+            //Login Details
+            Container(
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(10),
               decoration:BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
-                color: Colors.grey,
+                color: Colors.white,
               ),
-              child:Column(
+              child: Column(
                 children: [
-                  Text('Account Settings'),
+                  Text(
+                    'Login Details',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 10,),
                   Column(
                     children: [
-                      Row(
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('Notification'),
-                          // Switch(value: value, onChanged: onChanged)
-                          // trailing: Switch(
-                          //   value: _value,
-                          //   onChanged: (newValue) {
-                          //     setState(() {
-                          //       _value = newValue;
-                          //     });
-                          //     widget.onChanged(newValue);
-                          //   },
-                          //   activeColor: Colors.blueGrey[300],
-                          // ),
-                          Text('Two factor Authentication'),
+                          Text(
+                            'Email',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text('(UserEmail)')
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Password',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.grey,
+                            ),
+                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('(UserPassword)'),
+                              ElevatedButton(onPressed: (){}, child:Text('Change'))
+                            ],
+                          )
                         ],
                       )
                     ],
                   )
                 ],
-              )
-          ),
-          ElevatedButton(onPressed: () {}, child: Text('SIGN OUT'))
-        ],
+              ) ,
+            ),
+            SizedBox(height: 20,),
+            //Personal Details
+            Container(
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(10),
+              decoration:BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              child:Column(
+                children: [
+                  Text(
+                    'Personal Details',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                  Column(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Phone Number',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.grey,
+                            ),
+                            ),
+                          Text('(UserPhoneNumber)'),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Date of Birth',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.grey,
+                            ),
+                            ),
+                          Text('(UserDOB)')//TODO date and time button
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Address',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.grey,
+                            ),
+                            ),
+                          Text('(UserAddress)')
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ) ,
+            ),
+            SizedBox(height: 20,),
+            //Account Settings
+            Container(
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(10),
+                decoration:BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                ),
+                child:Column(
+                  children: [
+                    Text(
+                      'Account Settings',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text('Two factor Authentication'),
+                            //TODO: Switch
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                      ],
+                    )
+                  ],
+                )
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('SIGN OUT'))
+          ],
+        ),
       ),
     );
   }
